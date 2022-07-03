e4460707ab4566ac43a2d3c712db378549ffa3ee
Marketa <marketamajerova.6@gmail.com>
2022-07-03 18:28:21 +0200

    Adding container of tasks and plus button function

--- to-do_list.js
+++ to-do_list.js
@@ -1,1 +1,12 @@
 console.log('funguju');
+
+let containerTasks = document.getElementById('container-tasks');
+let input = document.querySelector('input');
+let button = document.querySelector('button');
+
+function addTask(){
+    let taskText = input.value;
+    let task = document.createElement('li');
+    task.innerText = taskText;
+    containerTasks.appendChild(task);
+}
